41f8eeb0c2f7585841ab607ed176b9d9c38b8cd3
amrocco <arocco89@gmail.com>
2019-04-12 12:36:27 -0400

    Adding self.included(base) method to injectable.rb
    Extends injectable's class methods, and prepends instance methods.

--- lib/injectable.rb
+++ lib/injectable.rb
@@ -8,4 +8,9 @@ require 'injectable/missing_dependencies_exception'
 
 module Injectable
   class Error < StandardError; end
+
+  def self.included(base)
+    base.extend(Injectable::ClassMethods)
+    base.prepend(Injectable::InstanceMethods)
+  end
 end
