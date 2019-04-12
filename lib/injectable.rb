1a8d0557650e5d127b5cff7c5a48a9468b96b0ac
amrocco <arocco89@gmail.com>
2019-04-12 12:34:24 -0400

    Updating injectable.rb to require files in lib/injectable

--- lib/injectable.rb
+++ lib/injectable.rb
@@ -1,6 +1,11 @@
-require "injectable/version"
+require 'injectable/version'
+require 'injectable/class_methods'
+require 'injectable/dependencies_graph'
+require 'injectable/dependencies_proxy'
+require 'injectable/dependency'
+require 'injectable/instance_methods'
+require 'injectable/missing_dependencies_exception'
 
 module Injectable
   class Error < StandardError; end
-  # Your code goes here...
 end
